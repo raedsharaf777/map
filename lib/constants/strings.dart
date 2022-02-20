8083d73a87dab96eda05889a5465b472e8c33dd2
Raed Sharaf <59477823+raedsharaf777@users.noreply.github.com>
2022-02-21 00:36:41 +0200

    Update strings.dart

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,7 +1,7 @@
 const loginScreen = '/login-screen';
 const otpScreen = '/otp-screen';
 const mapScreen = '/map-screen';
-const googleAPIKEY = 'AIzaSyBndxnysLnshmBkDhTQ1tVx-pSaDvA3cT0';
+const googleAPIKEY = '***************************';
 const suggestionBaseUrl =
     'https://maps.googleapis.com/maps/api/place/autocomplete/json';
 const placeLocationBaseUrl =
